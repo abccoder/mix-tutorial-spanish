17e01bfbc514812e958ecabbf3786f46b2fe275b
abccoder <alejandro_barbosa@yahoo.com>
2021-11-01 16:52:07 -0600

    Update install_tts_proto_stubs.sh
    Added some comments

--- tts/install_tts_proto_stubs.sh
+++ tts/install_tts_proto_stubs.sh
@@ -1,10 +1,11 @@
+# Check https://docs.mix.nuance.com/tts-grpc/v1/#grpc-setup for details
 unzip -o ../proto_files/nuance_tts_and_storage_protos.zip
 
-# Generate Python stubs from TTS proto files
+echo Generate Python stubs from TTS proto files
 python -m grpc_tools.protoc --proto_path=./ --python_out=./ --grpc_python_out=./ nuance/tts/v1/synthesizer.proto
 python -m grpc_tools.protoc --proto_path=./ --python_out=./ --grpc_python_out=./ nuance/tts/storage/v1beta1/storage.proto
 
-# Generate Python stubs from RPC proto files
+echo Generate Python stubs from RPC proto files
 python -m grpc_tools.protoc --proto_path=./ --python_out=./ nuance/rpc/error_details.proto
 python -m grpc_tools.protoc --proto_path=./ --python_out=./ nuance/rpc/status_code.proto
 python -m grpc_tools.protoc --proto_path=./ --python_out=./ nuance/rpc/status.proto

db0c624e2ee2bfc82635c52538ad5a8743411a6f
abccoder <alejandro_barbosa@yahoo.com>
2021-11-01 16:46:25 -0600

    Update install_asr_proto_stubs.sh
    Some comments added

--- asr/install_asr_proto_stubs.sh
+++ asr/install_asr_proto_stubs.sh
@@ -1,5 +1,8 @@
+# Check https://docs.mix.nuance.com/asr-grpc/v1/#grpc-setup for details
+
 unzip -o ../proto_files/nuance_asr_rpc_protos.zip
 
+echo Generate ASR client stubs from proto files
 python -m grpc_tools.protoc --proto_path=./ --python_out=./ --grpc_python_out=./ nuance/asr/v1/recognizer.proto
 python -m grpc_tools.protoc --proto_path=./ --python_out=./ nuance/asr/v1/resource.proto
 python -m grpc_tools.protoc --proto_path=./ --python_out=./ nuance/asr/v1/result.proto
